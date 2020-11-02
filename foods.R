aa3ae8ce7e39d08f1b18e11ab5b5d257fe9f93fe
ksirving <32795054+ksirving@users.noreply.github.com>
2020-11-02 11:25:30 -0800

    commit

--- foods.R
+++ foods.R
@@ -3,7 +3,7 @@
 
 # Let us know your favorite food!
 
-# My name is Glen Coco, and my favorite food is candy canes.
+# My name is Glen Coco, and my favorite food is popcorn.
 
 # My name is Leah, and my favorite food is coconut. 
 
